f7a09f51b7606775ed99633dd4e416b40d1356d3
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-01-02 16:42:46 +0000

    Set up paths and packages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dsv from '@rollup/plugin-dsv';
+import path from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), dsv()],
+	resolve: {
+		alias: {
+			$components: path.resolve("./src/components"),
+			$content: path.resolve("./src/content"),
+			$data: path.resolve("./src/data"),
+			$routes: path.resolve("./src/routes"),
+			$styles: path.resolve("./src/styles")
+		}
+	}
 });
