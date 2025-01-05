062e2d8c4fedd027a47752d1f3df139c864e3e3d
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-01-05 20:11:32 +0000

    Use my scroller again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import MattScroller from "../components/MattScroller.svelte";
     import Claude from "../components/Claude.svelte";
+    import GroupTest from "../components/GroupTest.svelte";
 </script>
 
-<Claude />
+<MattScroller />
