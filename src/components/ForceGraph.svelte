97a81f7d751658e5e8eb7e8e65bd62319cce17d1
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-01-05 09:40:14 +0000

    Trying to dynamically update graph

--- src/components/ForceGraph.svelte
+++ src/components/ForceGraph.svelte
@@ -8,28 +8,49 @@
    
     // let { data } = $props();
 
-    let { nodes, links } = $props();
+    let { nodes, links, index = 0 } = $props();
   
     const colorScale = scaleOrdinal(schemeCategory10);
   
     const linkForce = forceLink(links).id((d) => d.id);
-    const chargeForce = forceManyBody().strength(3);
-    const collideForce = forceCollide();
-    const centerForce = forceCenter();
-    const xForce = forceX().strength(0.1);
-    const yForce = forceY();
+    // const chargeForce = forceManyBody().strength(3);
+    // const collideForce = forceCollide();
+    const centerForce = forceCenter(20, 5);
+    // const xForce = forceX().strength(0.1);
+    // const yForce = forceY();
 
-    let alpha = 1;
-    const nodeStrokeWidth = 1
-    let groupBy = true;
-    $effect(() => reheatSimulation({ groupBy }));
+    let alpha = $state(1);
+    const nodeStrokeWidth = 1;
+
+    const linkedForce = {
+        link: linkForce,
+        charge: forceManyBody().distanceMin(0).distanceMax(1000).strength(-150),
+        collide: forceCollide(),
+        center: centerForce,
+    };
+
+    const unlinkedForce = {
+        y: forceY().strength(0.1),
+        x: forceX().strength(0.1),
+        charge: forceManyBody().strength(-30),
+        center: centerForce,
+    };
+
+    const graphForces = [unlinkedForce, linkedForce];
+
+    let currentForces = $derived(graphForces[index]);
 
     function reheatSimulation(args = {}) {
         const _ = args;
         alpha = 1.0;
     }
+
+    $effect(() => {
+        reheatSimulation({ index });
+    });
 </script>
 
+<p>Alpha = {alpha}</p>
 <div class="h-[680px] p-2">
     <Chart 
         data={nodes}
@@ -41,16 +62,11 @@
         let:width
         let:height
     >
+    {console.log(`height: ${height}; width: ${width}`)}
+    {console.log(currentForces)}
     <Svg center>
         <ForceSimulation
-            forces={{
-                link: linkForce,
-                charge: chargeForce,
-                y: yForce,
-                x: xForce.x((d) => (groupBy ? xGet(d) + xScale.bandwidth() / 2 : width / 2)),
-                charge: chargeForce,
-                center: centerForce.x(width / 2).y(height / 2),
-            }}
+            forces={graphForces[index]}
             cloneData
             bind:alpha
             let:nodes
@@ -62,7 +78,7 @@
         {/key}
 
         {#each nodes as node}
-            <Circle cx={node.x} cy={node.y} r={6} fill={colorScale(node.type)} />
+            <Circle cx={node.x} cy={node.y} r={10} fill={colorScale(node.type)} />
         {/each}
         </ForceSimulation>
     </Svg>
