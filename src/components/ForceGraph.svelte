bf2db925a8946afc3f62f155912327635de66402
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-01-06 08:09:45 +0000

    Different cluster layouts for mobile and desktop

--- src/components/ForceGraph.svelte
+++ src/components/ForceGraph.svelte
@@ -16,24 +16,57 @@
     const colorScale = scaleOrdinal(schemeCategory10);
 
     // There are 16 types of ingredient, so will show the clusters in a 4x4 grid
-    const typesXY = {
-        "Green & Grassy": [10, 0],
-        "Sulfurous": [30, 0],
-        "Fresh Fruity": [50, 0],
-        "Berry & Bush": [70, 0],
-        "Floral Fruity": [10, 30],
-        "Citrusy": [30, 30],
-        "Creamy Fruity": [50, 30],
-        "Earthy": [70, 30],
-        "Meaty": [10, 50],
-        "Woodland": [30, 50],
-        "Brine & Salt": [50, 50],
-        "Cheesy": [70, 50],
-        "Marine": [10, 70],
-        "Spicy": [30, 70],
-        "Mustardy": [50, 70],
-        "Roasted": [70, 70]
+    const typesXY_desktop = {
+        "Green & Grassy": [0.3, 0.3],
+        "Sulfurous": [0.4, 0.3],
+        "Fresh Fruity": [0.5, 0.3],
+        "Berry & Bush": [0.6, 0.3],
+
+        "Floral Fruity": [0.3, 0.4],
+        "Citrusy": [0.4, 0.4],
+        "Creamy Fruity": [0.5, 0.4],
+        "Earthy": [0.6, 0.4],
+
+        "Meaty": [0.3, 0.5],
+        "Woodland": [0.4, 0.5],
+        "Brine & Salt": [0.5, 0.5],
+        "Cheesy": [0.6, 0.5],
+
+        "Marine": [0.3, 0.6],
+        "Spicy": [0.4, 0.6],
+        "Mustardy": [0.5, 0.6],
+        "Roasted": [0.6, 0.6]
     };
+
+    // Put the clusters in a 3x5 grid (with an extra row for the roasted cluster)
+    // Change the order compared to desktop grid, so that the smallest clusters are down the right hand side
+    const typesXY_mobile = {
+        "Green & Grassy": [0.3, 0.1],
+        "Brine & Salt": [0.45, 0.1],
+        "Marine": [0.6, 0.1],
+
+        "Fresh Fruity": [0.3, 0.3],
+        "Berry & Bush": [0.45, 0.3],
+        "Spicy": [0.6, 0.3],
+
+        "Floral Fruity": [0.3, 0.4],
+        "Citrusy": [0.45, 0.4],
+        "Mustardy": [0.6, 0.4],
+
+        "Creamy Fruity": [0.3, 0.5],
+        "Earthy": [0.45, 0.5],
+        "Roasted": [0.6, 0.5],
+
+        "Meaty": [0.3, 0.6],
+        "Woodland": [0.45, 0.6],
+        "Sulfurous": [0.6, 0.6],
+
+        "Cheesy": [0.5, 0.7]
+    };
+
+    function getTypeXY(type, xy = 0, width) {
+        return (width < 768 ? typesXY_mobile[type][xy] : typesXY_desktop[type][xy]);
+    }
   
     const linkForce = forceLink(links).id((d) => d.id);
     const chargeForce = forceManyBody();
@@ -60,7 +93,7 @@
 </script>
 
 <p>Alpha = {alpha}</p>
-<div class="h-[300px] p-4 border rounded">
+<div class="h-[600px] p-4 border rounded">
     <Chart 
         data={nodes}
         x="type"
@@ -78,8 +111,8 @@
                 charge: chargeForce,
                 collide: collideForce,
                 center: centerForce,
-                ...(index < 3 && { x: xForce.strength(0.1).x((d) => (index == 1 ? typesXY[d.type][0] : width / 2)) }),
-                ...(index < 3 && { y: yForce.strength(0.1).y((d) => (index == 1 ? typesXY[d.type][1] : width / 2)) }),
+                ...(index < 3 && { x: xForce.strength(0.1).x((d) => (index == 1 ? getTypeXY(d.type, 0, width) * width : width / 2)) }),
+                ...(index < 3 && { y: yForce.strength(0.1).y((d) => (index == 1 ? getTypeXY(d.type, 1, width) * height : height / 2)) }),
                 ...(index >= 3 && { link: linkForce.id((d) => d.id).links(links) })
             }}
             alphaTarget={0.15}
