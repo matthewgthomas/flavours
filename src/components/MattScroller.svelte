6d0b431578cba73b78314e0f62e1d368b8b18037
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-01-11 18:08:32 +0000

    More story

--- src/components/MattScroller.svelte
+++ src/components/MattScroller.svelte
@@ -18,6 +18,12 @@
         );
     }
 
+    function filterLinksToNodes(links, nodeIds) {
+        return links.filter(link => 
+            nodeIds.includes(link.source) && nodeIds.includes(link.target)
+        );
+    }
+
     function filterNodes(nodes, links) {
         // Get unique ingredients from links
         const linkedIngredients = new Set([
@@ -29,8 +35,11 @@
         return nodes.filter(node => linkedIngredients.has(node.id));
     }
 
-    const filteredLinks = filterLinks(links, "Chocolate");
-    const filteredNodes = filterNodes(nodes, filteredLinks);
+    const chocolateLinks = filterLinks(links, "Chocolate");
+    const chocolateNodes = filterNodes(nodes, chocolateLinks);
+
+    const aniseLinks = filterLinks(links, "Anise");
+    const aniseNodes = filterNodes(nodes, aniseLinks);
 
     let currentNodes = nodes;
     let currentLinks = links;
@@ -78,8 +87,8 @@
 			},
             // Show chocolate's pairings
 			() => {
-				currentNodes = filteredNodes;
-				currentLinks = filteredLinks;
+				currentNodes = chocolateNodes;
+				currentLinks = chocolateLinks;
                 hiddenNodeOpacity = 0;
                 highlightedNodes = ["Chocolate"];
                 clusterByType = false;
@@ -87,8 +96,8 @@
 			},
             // Show chocolate's pairings, highlighting cinnamon
 			() => {
-				currentNodes = filteredNodes;
-				currentLinks = filteredLinks;
+				currentNodes = chocolateNodes;
+				currentLinks = chocolateLinks;
                 hiddenNodeOpacity = 0;
                 highlightedNodes = ["Chocolate", "Cinnamon"];
                 clusterByType = false;
@@ -96,8 +105,8 @@
 			},
             // Show chocolate's pairings, highlighting cauliflower and bacon
             () => {
-				currentNodes = filteredNodes;
-				currentLinks = filteredLinks;
+				currentNodes = chocolateNodes;
+				currentLinks = chocolateLinks;
                 hiddenNodeOpacity = 0;
                 highlightedNodes = ["Chocolate", "Cauliflower", "Bacon"];
                 clusterByType = false;
@@ -141,6 +150,70 @@
                 highlightedNodes = ["Swede"];
                 clusterByType = false;
                 sizeByDegree = true;
+			},
+            // All ingredients, clustered by type, sized by number of pairings
+            // highlighting apple
+            () => {
+				currentNodes = nodes;
+				currentLinks = [];
+                hiddenNodeOpacity = 0;
+                highlightedNodes = ["Apple"];
+                clusterByType = false;
+                sizeByDegree = true;
+			},
+            // Same as above
+            () => {
+				currentNodes = nodes;
+				currentLinks = [];
+                hiddenNodeOpacity = 0;
+                highlightedNodes = ["Apple"];
+                clusterByType = false;
+                sizeByDegree = true;
+			},
+            // Focus on anise
+			() => {
+				currentNodes = [{id: "Anise", type: "Green & Grassy"}];
+                currentLinks = [];
+                hiddenNodeOpacity = 0.1;
+                highlightedNodes = ["Anise"];
+                clusterByType = false;
+                sizeByDegree = false;
+			},
+            // Show anise's pairings
+			() => {
+				currentNodes = aniseNodes;
+				currentLinks = aniseLinks;
+                hiddenNodeOpacity = 0;
+                highlightedNodes = ["Anise"];
+                clusterByType = false;
+                sizeByDegree = false;
+			},
+            // Anise and oysters
+			() => {
+				currentNodes = aniseNodes;
+				currentLinks = aniseLinks;
+                hiddenNodeOpacity = 0;
+                highlightedNodes = ["Anise", "Oyster"];
+                clusterByType = false;
+                sizeByDegree = false;
+			},
+            // Anise and lemon
+			() => {
+				currentNodes = aniseNodes;
+				currentLinks = aniseLinks;
+                hiddenNodeOpacity = 0;
+                highlightedNodes = ["Anise", "Lemon"];
+                clusterByType = false;
+                sizeByDegree = false;
+			},
+            // Anise and swede
+			() => {
+				currentNodes = aniseNodes;
+				currentLinks = aniseLinks;
+                hiddenNodeOpacity = 0;
+                highlightedNodes = ["Anise", "Swede"];
+                clusterByType = false;
+                sizeByDegree = false;
 			},
         ]
     ];
@@ -240,7 +313,71 @@
         <section>
             <div class='col-medium'>
                 <p>
-                    Swede has the fewest pairings, with just 7 other ingredients: anise, beef, carrot, lamb, nutmeg, pork, and potato
+                    Swede has the fewest pairings, with just 7 other ingredients: anise, beef, carrot, lamb, nutmeg, pork, and potato.
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class='col-medium'>
+                <p>
+                    Apple deserves a special mention.
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class='col-medium'>
+                <p>
+                    Although apple isn't the most popular ingredient - barely squeaking into the top 20, based on number of pairings - it goes with every type of flavour, whether sulphurous, spicy, or citrussy.
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class='col-medium'>
+                <p>
+                    Some ingredients are often paired with the most popular ingredients.
+                </p>
+                <p>
+                    Anise is one. A  veritable - and <a href='https://www.bbcgoodfood.com/glossary/star-anise-glossary' target='_blank'>literal</a> - star among other star ingredients.
+                </p>
+                <p>
+                    <img src='https://images.immediate.co.uk/production/volatile/sites/30/2020/02/star-anise-ff5c1d1.jpg?quality=90&webp=true&resize=750,681' alt='Star anise. Source: BBC Good Food.' /> 
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class='col-medium'>
+                <p>
+                    In the book, Niki Segnit describes anise as a "very combinable flavour."
+                </p>
+                <p>
+                    It appears in star and seed form, in liquorice, fennel, and herbs like tarragon and chervil. Even some varieties of basil have liquorice notes (Thai basil in particular).
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class='col-medium'>
+                <p>
+                    If you're a fan of oysters, you can add a dash of <a href='https://www.healthyseasonalrecipes.com/tarragon-vinaigrette/' target='_blank'>tarragon vinaigrette</a>.
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class='col-medium'>
+                <p>
+                    Apparently anise seeds and lemon play nicely together in <a href='https://italianfoodforever.com/2012/02/lemon-anise-biscotti/' target='_blank'>biscotti</a>.
+                </p>
+                <p>
+                    The Flavour Thesaurus calls pastis - the <a href='https://en.wikipedia.org/wiki/Pastis' target='_blank'>anise-y aperitif</a> - an "odd but successful match for lemon in a sorbet."
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class='col-medium'>
+                <p>
+                    Even the relatively unpopular swede has a friend in anise.
+                </p>
+                <p>
+                    The Flavour Thesaurus recommends Hugh Fearnley-Whittingstall's <a href='https://www.google.co.uk/books/edition/The_River_Cottage_Meat_Book/a-r5GwAACAAJ?hl=en#ba_cen=lat_e7:%20515331909%0Alng_e7:%204294736586%0A&ba_loc=London%20E3%202PY' target='_blank'>leftover goose pasties</a>, which sound superb even to an anise-skeptic like me.
                 </p>
             </div>
         </section>
