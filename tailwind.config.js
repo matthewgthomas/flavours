d6529b5e7f3de54fc82995a9328f004f2dde3dc5
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-01-11 13:55:19 +0000

    Set custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ export default {
   ],
 
   theme: {
-    extend: {}
+    colors: {
+      'book-spine': '#e95a85',
+      'roasted': '#654d4b',  /* 4c5256 */
+      'meaty': '#a16f89',
+      'cheesy': '#f0eb54',
+      'earthy': '#7b6c66',
+      'mustardy': '#dac900',
+      'sulphurous': '#fade00',
+      'marine': '#03bab1',
+      'brine-salt': '#7de1c6',
+      'green-grassy': '#61cd3b',
+      'spicy': '#d0845e',
+      'woodland': '#62a14e',
+      'fresh-fruity': '#ed89b1',
+      'creamy-fruity': '#efcce1',
+      'citrussy': '#eceb01',
+      'bramble-hedge': '#765970',
+      'floral-fruity': '#b15582',
+    }
   },
 
   plugins: []
