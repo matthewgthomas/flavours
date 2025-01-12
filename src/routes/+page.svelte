0fe265718d41b85000933ca22f40a6dc442fd4c3
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-01-12 17:55:59 +0000

    Add meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,12 @@
     import MattScroller from "../components/MattScroller.svelte";
 </script>
 
+<svelte:head>
+    <title>What can you make with 99 ingredients? Exploring the Flavour Thesaurus | An interactive story by Matthew Gwynfryn Thomas</title>
+    <meta property="og:url" content="https://matthewgthomas.co.uk/flavours" />
+    <meta property="og:title" content="What can you make with 99 ingredients? Exploring the Flavour Thesaurus | An interactive story by Matthew Gwynfryn Thomas" />
+    <meta property="og:description" content="Exploring Niki Segnit's Flavour Thesaurus in an interactive scrollytelling story." />
+    <meta name="description" content="Exploring Niki Segnit's Flavour Thesaurus in an interactive scrollytelling story." />
+</svelte:head>
+
 <MattScroller />
