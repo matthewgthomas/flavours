293c62e98aa423791da00656905f32280f5be91f
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-01-17 11:26:09 +0000

    Rounded borders

--- src/components/Span.svelte
+++ src/components/Span.svelte
@@ -2,4 +2,4 @@
     let { colour, invert = false, children } = $props();
 </script>
 
-<span class={`${colour} ${invert ? 'text-white' : 'text-current'} pt-1 pl-1 pr-1`}>{@render children()}</span>
+<span class={`${colour} ${invert ? 'text-white' : 'text-current'} pt-1 pl-1 pr-1 rounded`}>{@render children()}</span>
